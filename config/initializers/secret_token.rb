550a69c7a0fc1e1611daa1abb9b1baea809fcd4c
Danielle <mdlee724@gmail.com>
2014-02-11 18:13:07 -0800

    add a bare rails app to track changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrossfitTracker::Application.config.secret_key_base = '96b43bd42aaf1f93c5cf1d01c145c22308370c7b19bce171a2bf529bd9930af8d03d79199904d9709ced73bc03eb9082c7eb804b9227f027811e71602134c58b'
